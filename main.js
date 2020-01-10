f38d7f5dde40c3789738c31db8d9993dfe55c0c7
Malena Rey <malerey@male.5d640511d691.sn.mynetname.net>
2020-01-10 18:29:33 -0300

    first card version

---
+++ main.js
@@ -0,0 +1,23 @@
+const productos = [
+  {
+    tipo: "zapato",
+    color: "azul",
+    precio: 3000
+  },
+  {
+    tipo: "zapato",
+    color: "negro",
+    precio: 2000
+  },
+  {
+    tipo: "zapatilla",
+    color: "negro",
+    precio: 3500
+  },
+  {
+    tipo: "zapatilla",
+    color: "azul",
+    precio: 2500
+  }
+]
+
