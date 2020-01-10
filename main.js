093a63c01bcdbe7d67bce11f00d6bf50583679c4
Malena Rey <malerey@male.5d640511d691.sn.mynetname.net>
2020-01-10 20:18:53 -0300

    cualquier nombre

--- main.js
+++ main.js
@@ -29,7 +29,28 @@ const productos = [
   }
 ]
 
+let contenedor = document.querySelector("section")
 
+
+for (let i = 0; i < productos.length; i++) {
+  const producto = productos[i];
+
+  let nombre = producto.nombre
+  let descripcion = producto.descripcion
+  let precio = producto.precio
+  let img = producto.img
+  contenedor.innerHTML = contenedor.innerHTML + `
+  <div class="card">
+  <div class="img"><img src="${img}"></div>
+  <div class="descripcion">
+  <h2>${nombre}</h2>
+  <p class="price">$ ${precio}</p>
+  <p>${descripcion}</p>
+  </div>
+  <button>Comprar</button>
+  </div>`
+  
+}
 
 
 
